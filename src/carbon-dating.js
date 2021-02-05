4c93bf6537ca273286fbf735d05335034edd6887
Si9num <78493168+Si9num@users.noreply.github.com>
2021-02-06 00:47:00 +0300

    feat: add conclusion

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity) {
+    if (typeof (sampleActivity) !== String) {
+        return false;
+    }
+    if (arguments.length == 0) {
+        return false;
+    }
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    return Math.ceil((MODERN_ACTIVITY/+sampleActivity)/k)
 };
