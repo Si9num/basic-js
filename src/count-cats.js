d9b05ce362c74a0d39b1551b19a9edb392111aad
Si9num <78493168+Si9num@users.noreply.github.com>
2021-02-06 00:45:45 +0300

    feat : add solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats( matrix) {
+   let arr = matrix.flat();
+    let sum = 0;
+    for (let a of arr) {
+        if (a == '^^') {
+            sum += 1;
+        }
+    }
+    return sum;
 };
