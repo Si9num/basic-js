cc2acf22c51a79a72b55a9c36cd907b21380b337
Si9num <78493168+Si9num@users.noreply.github.com>
2021-02-06 16:00:31 +0300

    feat:add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+    if (!Array.isArray(members)) {
+        return false;
+    }
+    let arr = [];
+    let arr1 = [];
+    members = members.filter(el => typeof el == "string");
+    for (let a of members) {
+        if (typeof (a) != String) {
+            arr.sort().push(a);
+        }
+
+    }
+    arr = arr.map(del => del.trim());
+    for (let b of arr) {
+        if (typeof b !== Number) {
+            arr1.push(b.charAt(0).toUpperCase());
+        }
+    }
+     
+    return arr1.sort().join("").trim().toUpperCase();
+    }
