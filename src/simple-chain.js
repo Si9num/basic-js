2f7d84cdf0428929dde7d851a670ddda59c85f66
Si9num <78493168+Si9num@users.noreply.github.com>
2021-02-06 20:54:19 +0300

    feat: add a solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+    chain:[],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      arguments.length == 0 ? this.chain.push("( )") : this.chain.push(`( ${value} )`);
+      
+      return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      if ( typeof position !== 'number' || !Number.isInteger(position) || (position <= 0) || (position > this.chain.length) ) {
+          this.chain = [];
+          throw new Error("incorrect");
+      }
+     
+     
+      this.chain.splice(position - 1, 1);
+  
+      
+      return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      this.chain.reverse();
+      return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      let finish = this.chain.join("~~");
+      this.chain= [];
+     return finish;
   }
 };
 
