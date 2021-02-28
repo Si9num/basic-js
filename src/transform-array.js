4aaab1dfeaa4bfa8d977f7512c9e363f1ce84f9d
Si9num <78493168+Si9num@users.noreply.github.com>
2021-02-28 19:20:14 +0300

    fix:fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -7,18 +7,21 @@ module.exports = function transform(arr) {
     };
         let result=[];
         let arr1 = ["--discard-next","--discard-prev","--double-next","--double-prev"];
-        for (let i=0; i<arr.length; i++){
-            if (arr[i]=="--discard-next"){
-                i++;
-            } else if (arr[i]=="--discard-prev"&&arr[i-2]!="--discard-next"){
-                result.pop();
-            } else if (arr[i]=="--double-next"&&arr[i+1]!=undefined) {
-                result.push(arr[i+1]);
-            } else if (arr[i]=="--double-prev"&&arr[i-1]!=undefined&&arr[i-2]!="--discard-next") {
-                result.push(arr[i-1]);
-            } else if (!arr1.includes(arr[i])){
-                result.push(arr[i]);
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == "--discard-next") {
+                arr.splice(i,2);
+            }
+            else if (arr[i] == "--discard-prev") {
+                arr.splice(i-1,2)
+            }
+            else if (arr[i] == "--double-next") {
+                arr.splice(i, 1);
+                arr[i + 1] == arr[i];
+            }
+            else if (arr[i] == "--double-prev") {
+                arr.splice(i, 1);
+                arr[i - 1] == arr[i];
             }
         }
-        return result;
+        return arr;
     };
