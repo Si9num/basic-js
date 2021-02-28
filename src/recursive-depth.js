32f3df36e1f798c39d1bd2e64598c0bc1d0b1121
Si9num <78493168+Si9num@users.noreply.github.com>
2021-02-28 18:37:54 +0300

    fix: fix solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,6 +2,9 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
     calculateDepth(arr) {
+        if (arr.length === 0) {
+            return 1;
+        }
         let res =0;
         for (let i = 0; i <arr.length; i++) {
             if (Array.isArray(arr[i])) {
