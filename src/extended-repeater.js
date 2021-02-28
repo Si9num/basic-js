9135982f3ff8ea8a7df3d253969ca2fcdd2b8e8d
Si9num <78493168+Si9num@users.noreply.github.com>
2021-02-28 19:57:35 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,1 +1,19 @@
 
+const CustomError = require("../extensions/custom-error");
+
+module.exports = function repeater(str, options) {
+    let res = [];
+    for (let i = 0; i < options.repeatTimes; i++) {
+        res.push(str);
+    }
+    if (options.separator !== undefined) {
+        res = res.join(options.separator);
+    }
+    else {
+        res = res.join("+");
+    }
+
+
+    return res;
+  
+};
