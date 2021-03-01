0250dbfe32379fbeeb90c5b2ea7926ae3ceee2fd
Si9num <78493168+Si9num@users.noreply.github.com>
2021-03-01 20:12:05 +0300

    fix: fix solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,19 +1,52 @@
 
+
+
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-    let res = [];
-    for (let i = 0; i < options.repeatTimes; i++) {
-        res.push(str);
+    let separator = "";
+    let addition = "";
+    let additionSeparator = "";
+    if (typeof options.repeatTimes == "undefined") {
+        options.repeatTimes = 1;
     }
-    if (options.separator !== undefined) {
-        res = res.join(options.separator);
+
+    if (typeof options.additionRepeatTimes == "undefined") {
+        options.additionRepeatTimes = 1;
+    }
+    if (options.addition != null) {
+        addition = options.addition;
     }
     else {
-        res = res.join("+");
+        addition = ""
     }
+    if (options.separator != null) {
+        separator = options.separator;
+    }
+    else {
+        separator = "+"
+    }
+    if (options.additionSeparator != null) {
+        additionSeparator = options.additionSeparator;
+    }
+    else {
+        additionSeparator = "|"
+    }
+    let res = [];
+    let res2 = [];
+   
+    for (let i = 0; i < options.repeatTimes; i++) { 
+        res.push(str + addition );
 
+    }
+    if (options.addition != null) {
+        for (let j = 0; j < options.additionRepeatTimes; j++) {
+            res2.push(options.addition);
 
-    return res;
-  
-};
+        }
+    }
+    
+   
+        return res.join(separator);
+
+}
