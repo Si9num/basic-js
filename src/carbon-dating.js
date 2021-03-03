dc1664f56d73ea6b938cece9c0253a2c4634a45a
Si9num <78493168+Si9num@users.noreply.github.com>
2021-03-03 12:07:28 +0300

    fix:fix solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,12 +4,10 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample( sampleActivity) {
-    if (typeof (sampleActivity) !== String) {
+    if (typeof (sampleActivity) !== "string" || !Number(sampleActivity) || sampleActivity < 0 || sampleActivity > 15) {
         return false;
     }
-    if (arguments.length == 0) {
-        return false;
-    }
-    let k = 0.693 / HALF_LIFE_PERIOD;
-    return Math.ceil((MODERN_ACTIVITY/+sampleActivity)/k)
+  
+    let k = Math.LN2 / HALF_LIFE_PERIOD;
+    return Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity))/k)
 };
