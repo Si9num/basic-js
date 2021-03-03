0ab409e4a2766a43c5cb72cd389a02c9e7b0c5ac
Si9num <78493168+Si9num@users.noreply.github.com>
2021-03-03 23:34:06 +0300

    fix: fix solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+    res: [],
+
+    getLength() {
+        return this.res.length;
+    },
+    addLink(value) {
+        typeof value !== "null" ? this.res.push(`( ${value} )`) : this.res.push("( )");
+        return this;
+    },
+    removeLink(position) {
+        if (typeof position !== "number" || position <= 0 || position > this.res.length) {
+            this.res = [];
+            throw new Error("Wrong type")
+        }
+        else {
+            if (position > 0) {
+                this.res.splice(position - 1, 1);
+                return this;
+
+            }
+            else {
+                this.res.splice(position - res.length, 1);
+                return this;
+            }
+        }
+    },
+    reverseChain() {
+        this.res.reverse();
+        return this;
+    },
+    finishChain() {
+        let end = this.res.join("~~");
+        this.res=[];
+        return end
+    }
 };
 
 module.exports = chainMaker;
